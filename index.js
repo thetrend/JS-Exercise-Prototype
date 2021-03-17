5cc0601781bda16e1e727f4e8c49f7c7aa2cec77
Grace de la Mora <grace.g.delamora@gmail.com>
2021-03-16 20:43:08 -0400

    Challenge 1 completed

--- index.js
+++ index.js
@@ -39,8 +39,23 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+    Person.prototype.eat = function(food) {
+      if (food || this.stomach.length < 10) {
+        this.stomach.push(food);
+      }
+    };
+    Person.prototype.poop = function() {
+      while (this.stomach.length > 0) {
+        this.stomach.pop();
+      }
+    };
+    Person.prototype.toString = function() {
+      return `${this.name}, ${this.age}`
+    };
   }
  
  
